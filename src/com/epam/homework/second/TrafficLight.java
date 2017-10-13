82f78add34bcc33cb9b87adefd5c97db0b68ac20
admin <olya.odintsova@gmail.com>
2017-10-13 11:23:10 +0300

    minor corrections

--- src/com/epam/homework/second/TrafficLight.java
+++ src/com/epam/homework/second/TrafficLight.java
@@ -3,31 +3,26 @@ package com.epam.homework.second;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- *
- * напиши javadoc
- */
 public class TrafficLight {
 
     public static void main(String[] args) throws IOException {
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of minutes: ");
+        String line = scanner.nextLine();
+        int numberMinutes = validate(line) % 10;
 
-            System.out.print("Enter the number of minutes: ");
-            String line = scanner.nextLine();
-            int numberMinutes = validate(line) % 10;
-
-            if (numberMinutes >= 0 && numberMinutes < 2) {
-                System.out.println("The traffic light is Red.");
-            } else if (numberMinutes >= 2 && numberMinutes < 5) {
-                System.out.println("The traffic light is Yellow.");
-            } else if (numberMinutes >= 5 && numberMinutes < 10) {
-                System.out.println("The traffic light is Green!");
-            }
+        //2 минуты красный, 3 минуты жёлтый, 5 минут зелёный
+        if (numberMinutes >= 0 && numberMinutes < 2) {
+            System.out.println("The traffic light is Red.");
+        } else if (numberMinutes >= 2 && numberMinutes < 5) {
+            System.out.println("The traffic light is Yellow.");
+        } else if (numberMinutes >= 5 && numberMinutes < 10) {
+            System.out.println("The traffic light is Green!");
         }
+
     }
 
-
-    //проверяем валидность
+    //проверяем валидность введённого числа
     private static int validate(String line) {
         try {
             // Проверить что int
@@ -40,7 +35,7 @@ public class TrafficLight {
                 throw new NumberFormatException("Got a negative value");
             }
         } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("You entered the wrong number!!!");
+            throw new IllegalArgumentException("The number is not correct!!!");
         }
     }
 
