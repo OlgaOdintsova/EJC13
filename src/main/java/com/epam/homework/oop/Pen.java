fd147b900bb134ed047613827427e459dd5d75cc
OlgaOdintsova <olya.odintsova@gmail.com>
2017-11-13 00:40:12 +0300

    ООП. Задание 1

---
+++ src/main/java/com/epam/homework/oop/Pen.java
@@ -0,0 +1,50 @@
+package com.epam.homework.oop;
+
+public class Pen {
+    private int price;
+    private String name;
+
+    public Pen(int price, String name){
+        this.price = price;
+        this.name = name;
+    }
+
+    public boolean equals(Object obj) {
+        if (null == obj){
+            return false;
+        }
+
+        if (this ==obj){
+            return true;
+        }
+
+        if (obj.getClass() != Pen.class){
+            return false;
+        }
+
+        Pen pen = (Pen) obj;
+        if (price != pen.price) {
+            return false;
+        }
+
+        if (null == name) {
+            return (name == pen.name);
+        } else {
+            if (!name.equals(pen.name)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = price;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return getClass().getName() + "@" + "price: " + price + ", name: " + name;
+    }
+}
